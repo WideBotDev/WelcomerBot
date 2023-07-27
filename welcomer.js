7f7e6b1b44648fef9342547dc5b438b14d3fedad
VexRito <43346473+VexRito@users.noreply.github.com>
2023-07-27 12:52:10 +0200

    Update welcomer.js

--- welcomer.js
+++ welcomer.js
@@ -1,5 +1,21 @@
 /*
 
+   Copyright 2023 Pratelli Mattia
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+-------------------
+
 Dependencies: discord.js@^13.14.0
 
 This bot will send a welcome message when a user joins the server and a leave message when a user leaves the server.
